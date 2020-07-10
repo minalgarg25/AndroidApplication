c9361495f7d1b95980fdde89883a2bcfa644bafa
Minal <minal.251298@gmail.com>
2020-07-10 16:11:32 +0530

    Whatsapp Opener

---
+++ app/src/main/java/com/example/whatsappopener/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.whatsappopener
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.text.isDigitsOnly
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var number:String = "0"
+
+        if (intent.action == Intent.ACTION_PROCESS_TEXT) {
+            number = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString() //takes number from Intent and store it in variable
+        }
+
+        if(number.isDigitsOnly()) {
+            startWhatsapp(number)
+        }
+        else {
+            Toast.makeText(this,"please check the number",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun startWhatsapp(number: String) {
+        //to open Whatsapp we will use whatsapp api
+
+        val intent=Intent(Intent.ACTION_VIEW)
+        intent.setPackage("com.whatsapp")
+       val data: String= if (number[0] == '+') {
+            number.substring(1) //+91 8708307227
+        }
+        else if(number.length == 10) {
+            "91"+number
+        }
+        else {
+           number
+       }
+        intent.data= Uri.parse("https://wa.me/$data")
+        if(packageManager.resolveActivity(intent,0) != null)  { //agr whatsapp he nhi hai
+            startActivity(intent)
+        }
+        else {
+            Toast.makeText(this,"Please install Whatsapp",Toast.LENGTH_LONG).show()
+        }
+        finish()
+
+    }
+}
